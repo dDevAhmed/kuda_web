d989a854aebbe4c5d603b707cd6aac038b91c245
Ahmed <ahmedmahmud1994@hotmail.com>
2024-07-30 23:58:32 +0100

    testimonial section - carousel

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// testimonail carousel
 document.addEventListener('DOMContentLoaded', () => {
-    const carousel = document.querySelector('.testimonial-container');
-    const cards = carousel.children;
+    const testimonialContainer = document.querySelector('.testimonial-container');
+    const cards = testimonialContainer.children;
     const cardCount = cards.length;
 
     for (let i = 0; i < cardCount; i++) {
         const clone = cards[i].cloneNode(true);
-        carousel.appendChild(clone);
+        testimonial - container.appendChild(clone);
     }
 });
