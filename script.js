b59cac6e49768db83011df324f6512f27e93487b
Ahmed <ahmedmahmud1994@hotmail.com>
2024-08-02 00:48:04 +0100

    nav overlay

--- script.js
+++ script.js
@@ -7,4 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
         const clone = cards[i].cloneNode(true);
         testimonial - container.appendChild(clone);
     }
+
+});
+// nav dropdown
+
+let nav = document.querySelector('nav');
+let navOverlay = document.getElementById('nav-overlay');
+let dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    let dropdownContent = dropdown.querySelector('.dropdown-content');
+    dropdown.addEventListener('mouseenter', () => {
+        dropdownContent.style.display = 'block';
+        navOverlay.style.display = 'block';
+        nav.style.backgroundColor = 'rgba(64, 25, 109, 0)'; // Ensure it's transparent or use a color if needed
+    });
+    dropdown.addEventListener('mouseleave', () => {
+        dropdownContent.style.display = 'none';
+        navOverlay.style.display = 'none';
+        nav.style.backgroundColor = '#fff'; // Reset to original color
+    });
 });
